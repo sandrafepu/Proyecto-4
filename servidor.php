2370267fb449f5cce4b2d5997b2c37951d2d28d9
sandra <sandrafepu@gmail.com>
2025-02-19 17:45:47 +0100

    Comprobando comprobaciones

--- servidor.php
+++ servidor.php
@@ -1,5 +1,25 @@
 <?php
 header('Content-Type: application/json');
-$datos = file_get_contents("datos.json");
-echo $datos;
+
+// Comprobaciones
+if (!file_exists("datos.json")) {
+    echo json_encode(["error" => "El archivo datos.json no existe."]);
+    exit;
+}
+
+$datos_json = file_get_contents("datos.json");
+
+if (empty($datos)) {
+    echo json_encode(["error" => "El archivo datos.json esta vacio."]);
+    exit;
+}
+
+$datos_json = json_decode($datos,true);
+
+if (json_last_error() !== JSON_ERROR_NONE) {
+    echo json_encode(["error" => "El archivo datos.json contiene un json invalido."]);
+    exit;
+}
+
+echo json_encode($datos_json);
 ?>
